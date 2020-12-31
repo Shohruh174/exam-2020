92e5aba83c6e7f48f1ad5df7feac0d30fae79f5d
Shohruh174 <shbotirov227@gmail.com>
2020-12-31 08:00:34 +0500

    carusel qismi boldi

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+var mySwiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  slidesPerView: 5,
+  spaceBetween: 30,
+  centeredSlides: true,
+  pagination: {
+    el: '.swiper-pagination',
+  },
